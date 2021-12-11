9006eabfd1c939677969984aa130d8a73ef1dbe5
Khemi <khemi@Khemis-Laptop.local>
2021-12-11 14:05:24 +0000

    mediaqqueries changed to 961px

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -9,8 +9,8 @@ const Card = (props) => {
       <img className="Card__picture" src={picture} alt={name} />
       <h2 className="Card__name">{name}</h2>
       <h3 className="Card__tag">{tag}</h3>
-      <h4 className="Card__date">{date}</h4>
-      <h5 className="Card__abv">ABV: {abv}</h5>
+      <h4 className="Card__abv">ABV: {abv}</h4>
+      <h5 className="Card__date">{date}</h5>
       <h6 className="Card__ph">pH: {ph}</h6>
     </div>
   );
