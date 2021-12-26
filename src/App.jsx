16dfa7ff1a2d5ead6a5703c7b253f8bf11d45968
Khemi <khemi@Khemis-Laptop.local>
2021-12-26 22:37:49 +0000

    deleted styling and changed the search function to search the api and added a catch

--- src/App.jsx
+++ src/App.jsx
@@ -10,18 +10,29 @@ const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
   let [urlLink, setUrlLink] = useState("");
   const [unfilteredObject, setUnfilteredObject] = useState([]);
-  const [highAcidity, setHighAcidity] =useState([]);
+  const [highAcidity, setHighAcidity] = useState([]);
   const [highAcidityCheck, setHighAcidityCheck] = useState(false);
+  const [searching, setSearching] = useState("");
+  const [reset, setReset] = useState(false);
 
   const handleInput = (event) => {
-    const cleanInput = event.target.value.toLowerCase();
+    const cleanInput = event.target.value;
+    if (cleanInput.length===0) {
+      setReset(true);
+      setBeerArray(unfilteredObject);
+    } else {
+      setReset(false);
+    }
     setSearchTerm(cleanInput);
+    if (searchTerm.length !== 0) {
+      setSearching(`beer_name=${searchTerm.replace(" ", "_").toLowerCase()}&`);
+    }
   };
 
-  const filteredBeers = beerArray.filter((beer) => {
-    const beerLowerCase = beer.name.toLowerCase();
-    return beerLowerCase.includes(searchTerm);
-  });
+  // const filteredBeers = beerArray.filter((beer) => {
+  //   const beerLowerCase = beer.name.toLowerCase();
+  //   return beerLowerCase.includes(searchTerm);
+  // });
 
   const handleCheck_high_ABV = (event) => {
     if (event.target.checked) {
@@ -37,7 +48,6 @@ const App = () => {
         setUrlLink("");
       }
     }
-    console.log(urlLink);
   };
 
   const handleCheck_classic_range = (event) => {
@@ -55,12 +65,11 @@ const App = () => {
         setUrlLink("");
       }
     }
-    console.log(urlLink);
   };
 
   const handleCheck_high_acidity = (event) => {
-    setHighAcidityCheck(!highAcidityCheck)
-    console.log(highAcidityCheck)
+    setHighAcidityCheck(!highAcidityCheck);
+    console.log(highAcidityCheck);
 
     console.log(urlLink);
     event.target.checked
@@ -70,10 +79,13 @@ const App = () => {
 
   //ACCESSING API
   useEffect(() => {
-    const URL = `https://api.punkapi.com/v2/beers?${urlLink}&per_page=80`;
+    const URL = `https://api.punkapi.com/v2/beers?${searching}${urlLink}&per_page=80`;
     fetch(URL)
       .then((response) => {
-        return response.json();
+        if (response.ok) {
+          return response.json();
+        }
+        return Promise.reject(response);
       })
       .then((beerObject) => {
         setBeerArray(beerObject);
@@ -81,12 +93,19 @@ const App = () => {
         const high_acidity = beerObject.filter(
           (beer) => beer.ph < 4 && beer.ph !== null
         );
-        if (highAcidityCheck===true) {
-          setBeerArray(high_acidity)
+        if (highAcidityCheck === true) {
+          setBeerArray(high_acidity);
         }
         setHighAcidity(high_acidity);
+      })
+      .catch((error) => {
+        if(error) {
+          if (reset===false) {
+            setBeerArray([]);
+          }
+        }       
       });
-  }, [urlLink, highAcidityCheck]);
+  }, [urlLink, highAcidityCheck, searching, reset]);
 
   return (
     <div className="App">
@@ -94,13 +113,13 @@ const App = () => {
       <div className="App_main">
         <NavBar
           handleInput={handleInput}
-          results={filteredBeers.length}
+          results={beerArray.length}
           filterFunction1={handleCheck_high_ABV}
           filterFunction2={handleCheck_classic_range}
           filterFunction3={handleCheck_high_acidity}
           searchTerm={searchTerm}
         />
-        <CardList BeerArr={filteredBeers} />
+        <CardList BeerArr={beerArray} />
       </div>
     </div>
   );
