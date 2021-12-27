e572f304697bca39d99c741306e2a1d5a970badd
Khemi <khemi@Khemis-Laptop.local>
2021-12-27 17:45:12 +0000

    mobile styling complete

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,16 +2,18 @@ import React from "react";
 import "./Card.scss";
 
 const Card = (props) => {
-  const {picture, name, tag, date, ph, abv } = props;
+  const { picture, name, tag, date, ph, abv } = props;
 
   return (
     <div className="card__container">
       <img className="card--picture" src={picture} alt={name} />
       <h2 className="card--name">{name}</h2>
       <h3 className="card--tag">{tag}</h3>
-      <h4 className="card--abv">ABV: {abv}</h4>
-      <h5 className="card--date">{date}</h5>
-      <h6 className="card--ph">pH: {ph}</h6>
+      <div className="card__data">
+        <h5 className="data card--abv">ABV: {abv}</h5>
+        <h5 className="data card--date">{date}</h5>
+        <h5 className="data card--ph">pH: {ph}</h5>
+      </div>
     </div>
   );
 };
